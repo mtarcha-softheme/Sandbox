8b4866ca697da1e83e31960cf1b771bfa7cdf337
Myroslava Tarvcha <tarcham1993@gmail.com>
2015-07-13 00:13:21 +0300

    Add AlgoLib progect
    Add AlgoLib_1 project whith contain SearchAlgorithms with some tests.
    
    AlgoLib

---
+++ Study/AlgoLib/AlgoLib_1/SearchAlgorithms.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoLib_1
+{
+    public class SearchAlgorithms
+    {
+        public static TSource BinarySearch<Tkey, TSource>(IList<TSource> list, Tkey key, Func<TSource, Tkey> getKey)
+            where Tkey : IComparable<Tkey>
+        {
+            Validate(list, getKey);
+
+            return BinarySearchInternal(list, key, getKey, 0, list.Count - 1);
+        }
+
+        public static TSource BinarySearch<Tkey, TSource>(TSource[] array, Tkey key, Func<TSource, Tkey> getKey)
+            where Tkey : IComparable<Tkey>
+        {
+            Validate(array, getKey);
+
+            return BinarySearchInternal(array, key, getKey, 0, array.Length - 1);
+        }
+
+        private static TSource BinarySearchInternal<Tkey, TSource>(IList<TSource> list, Tkey key, Func<TSource, Tkey> getKey, int leftIndex, int rightIndex)
+            where Tkey : IComparable<Tkey>
+        {
+            var mid =  (leftIndex + rightIndex) / 2;
+
+            if(leftIndex == rightIndex)
+            {
+                if(getKey(list[mid]).CompareTo(key) != 0)
+                {
+                    throw new InvalidOperationException("Value is not found.");
+                }
+                else
+                {
+                    return list[mid];
+                }
+            }
+
+            if (getKey(list[mid]).CompareTo(key) < 0)
+            {
+                return BinarySearchInternal(list, key, getKey, mid + 1, rightIndex);
+            }
+
+            return BinarySearchInternal(list, key, getKey, leftIndex, mid);
+        }
+
+        private static void Validate<Tkey, TSource>(IEnumerable<TSource> list, Func<TSource, Tkey> getKey)
+             where Tkey : IComparable<Tkey>
+        {
+            TSource previous = list.First();
+            list.GetEnumerator().MoveNext();
+
+            foreach (var item in list)
+            {
+                if (getKey(item).CompareTo(getKey(previous)) < 0)
+                {
+                    throw new ArgumentException("List or array is not sorted by encreasing!");
+                }
+
+                previous = item;
+            }
+        }
+    }
+}
---
+++ Study/AlgoLib/AlgoLib_1/SearchAlgorithmsTests.cs
@@ -0,0 +1,88 @@
+﻿// <copyright company="Tarcha & Ivchenko Company">
+//       Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <author>Myroslava Tarcha</author>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace AlgoLib_1
+{
+    [TestFixture]
+    public class SearchAlgorithmsTests
+    {
+        private IList<MyTestClass> _testList;
+        private MyTestClass[] _testArray;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testList = new List<MyTestClass>();
+            _testArray = new MyTestClass[100];
+
+            for (var i = 0; i < 100; i++)
+            {
+                var item = new MyTestClass() { Key = i, Value = i };
+                _testList.Add(item);
+                _testArray[i] = item;
+            }
+        }
+
+        [TestCase(1)]
+        [TestCase(20)]
+        [TestCase(50)]
+        [TestCase(75)]
+        [TestCase(99)]
+        public void BinarySearch_InArraySuccessTest(int key)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var result = SearchAlgorithms.BinarySearch(_testArray, key, e => e.Key);
+            sw.Stop();
+            Console.WriteLine("Elapsed time = {0} ticks.", sw.ElapsedTicks);
+
+            Assert.AreEqual(result.Value, key);
+        }
+
+        [TestCase(1)]
+        [TestCase(20)]
+        [TestCase(50)]
+        [TestCase(75)]
+        [TestCase(99)]
+        public void BinarySearch_InListSuccessTest(int key)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var result = SearchAlgorithms.BinarySearch(_testList, key, e => e.Key);
+            sw.Stop();
+            Console.WriteLine("Elapsed time = {0} ticks.", sw.ElapsedTicks);
+
+            Assert.AreEqual(result.Value, key);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [Test]
+        public void BinarySearch_NotSortedArrayTest()
+        {
+            _testArray[50] = new MyTestClass { Key = 5 };
+            SearchAlgorithms.BinarySearch(_testArray, 2, e => e.Key);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [Test]
+        public void BinarySearch_KeyDoesNotExistTest()
+        {
+            SearchAlgorithms.BinarySearch(_testArray, 100, e => e.Key);
+        }
+
+        private class MyTestClass
+        {
+            public int Key { get; set; }
+
+            public int Value { get; set; }
+        }
+    }
+}
